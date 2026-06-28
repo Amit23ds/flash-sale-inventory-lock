5740b56ec6fa1a04b719a36163419aca05539b19
Amit Kumar Yadav <157636243+Amit23ds@users.noreply.github.com>
2026-06-28 14:50:19 +0530

    Update ProductServiceImpl.java

--- src/main/java/com/example/demo/service/impl/ProductServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ProductServiceImpl.java
@@ -1,6 +1,5 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.dto.StockResponse;
 import com.example.demo.entity.Product;
 import com.example.demo.exception.ProductNotFoundException;
 import com.example.demo.repository.ProductRepository;
@@ -18,14 +17,5 @@ public class ProductServiceImpl implements ProductService {
     public Product getProductById(Long productId) {
         return productRepository.findById(productId)
                 .orElseThrow(() -> new ProductNotFoundException("Product not found"));
-    }
-
-    @Override
-    public StockResponse getAvailableStock(Long productId) {
-        Product product = getProductById(productId);
-        return StockResponse.builder()
-                .productId(product.getId())
-                .availableStock(product.getAvailableStock())
-                .build();
     }
 }
