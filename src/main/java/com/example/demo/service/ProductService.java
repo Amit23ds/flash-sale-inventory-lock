5e7ffc4562ec709bbdf3af77e1bad95aebf4ee1c
Amit Kumar Yadav <157636243+Amit23ds@users.noreply.github.com>
2026-06-28 14:49:56 +0530

    Update ProductService.java

--- src/main/java/com/example/demo/service/ProductService.java
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -1,11 +1,8 @@
 package com.example.demo.service;
 
-import com.example.demo.dto.StockResponse;
 import com.example.demo.entity.Product;
 
 public interface ProductService {
 
     Product getProductById(Long productId);
-
-    StockResponse getAvailableStock(Long productId);
 }
