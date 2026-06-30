481fd8200f2d196069a1ab332f347a115ca1ff20
Amit Kumar Yadav <yadavsahab248@gmail.com>
2026-07-01 00:20:55 +0530

    feat: add Product and Order entities with JPA optimistic locking

---
+++ src/main/java/com/example/demo/entity/Product.java
@@ -0,0 +1,38 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Version;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "product_name", nullable = false, length = 255)
+    private String productName;
+
+    @Column(name = "available_stock", nullable = false)
+    private Integer availableStock;
+
+    @Version
+    @Column(name = "version", nullable = false)
+    private long version;
+}
